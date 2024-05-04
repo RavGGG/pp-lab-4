d06c5a01c9c50702a0cb5e05ca35956a567ac138
Rafal <rafalgad@wp.pl>
2024-05-04 15:14:19 +0200

    zad2

--- Company.java
+++ Company.java
@@ -1,22 +1,28 @@
+import employees.Employee;
+import employees.Manager;
+import employees.Worker;
+
 public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
     
-        employees[0] = new Employee("Rafał Nowak", 4500.00);
-        employees[1] = new Employee("Adam Gabacki", 5000.00);
+        employees[0] = new Manager("Rafał Nowak", 4500.00,0);
+        employees[1] = new Worker("Adam Gabacki", 5000.00,"HR Specialist");
         employees[2] = new Employee("Katarzyna Nowicka", 3200.00);
         employees[3] = new Employee("Michał Babacki", 3600.00);
-        employees[4] = new Employee("Teresa Witkowska", 3700.00);
+        employees[4] = new Worker("Teresa Witkowska", 3700.00,"Developer");
 
         Employee thirdEmployee = employees[3];
 
         employees[3].setSalary(4100);
 
-        System.out.println(thirdEmployee);  
+        System.out.println("All employees:");
+        for (Employee emp : employees) {
+            System.out.println(emp.toString());
    
     }
 
 
-
+    }
 
 }
---
+++ employees/Worker.java
@@ -0,0 +1,28 @@
+package employees;
+
+public class Worker extends Employee  {
+
+    public String position;
+
+
+    public Worker(String fullName, double salary, String position) {
+        super(fullName, salary);
+        this.position = position;
+    }
+
+
+    public String getPosition() {
+        return this.position;
+    }
+
+
+    public void setPosition (String newPosition) {
+        this.position = newPosition;
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Worker: " + getName() + ", Salary: " + getSalary() + ", Position: " + position;
+    }
+}
