91d54aeac514ea08fb627545ab62d5ec77feb5f1
Rafal <rafalgad@wp.pl>
2024-05-04 14:34:19 +0200

    zad1

--- Company.java
+++ Company.java
@@ -6,15 +6,15 @@ public class Company {
         employees[1] = new Employee("Adam Gabacki", 5000.00);
         employees[2] = new Employee("Katarzyna Nowicka", 3200.00);
         employees[3] = new Employee("Michał Babacki", 3600.00);
-        employees[4] = new Employee("Martyna Witkowska", 3700.00);
+        employees[4] = new Employee("Justyna Witkowska", 3700.00);
 
         Employee thirdEmployee = employees[3];
+
         employees[3].setSalary(4100);
 
-        System.out.println(thirdEmployee);
-
+        System.out.println(thirdEmployee);  
+   
     }
-
 
 
 
